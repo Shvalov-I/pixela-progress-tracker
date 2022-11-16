7b8dc074696169b6b0b11f7b5e1401e3db130438
Ivan <shvalov.i@gmail.com>
2022-11-16 14:16:24 +0700

    add User models

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
-import sqlalchemy
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'Pixela_User'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), unique=True)
+    token = Column(String(50), unique=True)
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username}"
