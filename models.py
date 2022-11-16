0b6ed94dc89a2b65b520f532c3a64724254d2125
Ivan <shvalov.i@gmail.com>
2022-11-16 14:26:21 +0700

    add Graphs models

--- models.py
+++ models.py
@@ -5,10 +5,17 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'Pixela_User'
+    __tablename__ = 'pixela_users'
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True)
     token = Column(String(50), unique=True)
 
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}"
+
+
+class Graphs(Base):
+    __tablename__ = 'pixela_users_graphs'
+    id = Column(Integer, primary_key=True)
+    graph_name = Column(String(30))
+    user_id = Column(Integer, ForeignKey('pixela_users.id'))
