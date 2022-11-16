7b8c401da3e7b2ecf8057cbe50efb420d27ea245
Ivan <shvalov.i@gmail.com>
2022-11-16 14:29:31 +0700

    add relationship with Users and Graphs

--- models.py
+++ models.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import declarative_base, relationship
 Base = declarative_base()
 
 
-class User(Base):
+class Users(Base):
     __tablename__ = 'pixela_users'
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True)
     token = Column(String(50), unique=True)
-
+    graphs = relationship("Graphs", back_populates='user')
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}"
 
@@ -18,4 +18,5 @@ class Graphs(Base):
     __tablename__ = 'pixela_users_graphs'
     id = Column(Integer, primary_key=True)
     graph_name = Column(String(30))
+    user = relationship("Users", back_populates='graphs')
     user_id = Column(Integer, ForeignKey('pixela_users.id'))
