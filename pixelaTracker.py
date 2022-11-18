7a7840b659b0b366dea0b302bd2eb90d3f22bd79
Ivan <shvalov.i@gmail.com>
2022-11-18 11:20:49 +0700

    add class PixelaGraph

--- pixelaTracker.py
+++ pixelaTracker.py
@@ -72,78 +72,67 @@ class PixelaUser:
         else:
             raise AttributeError(f'User "{self.USERNAME}" do not exists')
 
-# class PixelaGraph:
-#     USERNAME = os.environ['PIXELA_USERNAME']
-#     TOKEN = os.environ['PIXELA_TOKEN']
-#
-#     PIXEL_ENDPOINT = f"https://pixe.la/v1/users/{USERNAME}/graphs/graph1"
-#
-#     HEADERS = {
-#         "X-USER-TOKEN": TOKEN,
-#     }
-#
-#     def __init__(self, username: str, token: str, graph_name: str):
-#         self.USERNAME = username
-#         self.TOKEN = token
-#         self.GRAPH_NAME = graph_name
-#         self.HEADERS = {"X-USER-TOKEN": self.TOKEN}
-#         self.PIXEL_ENDPOINT = f"https://pixe.la/v1/users/{self.USERNAME}/graphs/{self.GRAPH_NAME}"
-#
-#     def post_progress_pixel(user_date: str = dt.datetime.today().strftime('%Y%m%d')):
-#         """Asks the value of the user and changes the progress by user date"""
-#         correct_input = False
-#         new_score = 0
-#         while not correct_input:
-#             try:
-#                 new_score = float(input('Введите на какое значение вы хотите изменить(в часах): '))
-#             except ValueError:
-#                 print("Вы ввели некорректные данные. Попробуйте ещё раз.")
-#             else:
-#                 correct_input = True
-#
-#         new_score_params = {
-#             'date': user_date,
-#             'quantity': str(new_score),
-#         }
-#         response = requests.post(f'{PIXEL_ENDPOINT}', headers=HEADERS, json=new_score_params)
-#
-#     def get_progress_pixel(user_date: str):
-#         """Returns the pixel value of the given day"""
-#         response = requests.get(f'{PIXEL_ENDPOINT}/{user_date}', headers=HEADERS)
-#         return response.json()['quantity']
-#
-#     def change_progress_pixel():
-#
-#         today = dt.datetime.today().strftime('%Y%m%d')
-#
-#         yesterday = dt.datetime.today() - dt.timedelta(days=1)
-#         yesterday = yesterday.strftime('%Y%m%d')
-#
-#         day_before_yesterday = dt.datetime.today() - dt.timedelta(days=1)
-#         day_before_yesterday = day_before_yesterday.strftime('%Y%m%d')
-#
-#         user_date_id = None
-#
-#         correct_input = False
-#         while not correct_input:
-#             try:
-#                 user_date_id = int(input('Введите код дня, который вы хотите изменить '
-#                                          '(Сегодня - 0, Вчера - 1, Позавчера - 2): '))
-#                 if user_date_id in (0, 1, 2):
-#                     correct_input = True
-#                 else:
-#                     print('Вы ввели неправильный индекс дня. Попробуйте ещё раз.')
-#             except ValueError:
-#                 print('Некорректный ввод. Попробуйте ещё раз.')
-#
-#         if user_date_id == 0:
-#             print(f'Ваш прогресс сегодня составляет {get_progress_pixel(today)}')
-#             post_progress_pixel(today)
-#         elif user_date_id == 1:
-#             print(f'Ваш прогресс вчера составляет {get_progress_pixel(yesterday)}')
-#             post_progress_pixel(yesterday)
-#         elif user_date_id == 2:
-#             print(f'Ваш прогресс позавчера составляет {get_progress_pixel(day_before_yesterday)}')
-#             post_progress_pixel(day_before_yesterday)
-#
-#     change_progress_pixel()
+
+class PixelaGraph(PixelaUser):
+    def __init__(self, username: str, graph_name: str):
+        super().__init__(username)
+        self.GRAPH_NAME = graph_name
+
+    # def post_progress_pixel(user_date: str = dt.datetime.today().strftime('%Y%m%d')):
+    #     """Asks the value of the user and changes the progress by user date"""
+    #     correct_input = False
+    #     new_score = 0
+    #     while not correct_input:
+    #         try:
+    #             new_score = float(input('Введите на какое значение вы хотите изменить(в часах): '))
+    #         except ValueError:
+    #             print("Вы ввели некорректные данные. Попробуйте ещё раз.")
+    #         else:
+    #             correct_input = True
+    #
+    #     new_score_params = {
+    #         'date': user_date,
+    #         'quantity': str(new_score),
+    #     }
+    #     response = requests.post(f'{PIXEL_ENDPOINT}', headers=HEADERS, json=new_score_params)
+    #
+    # def get_progress_pixel(user_date: str):
+    #     """Returns the pixel value of the given day"""
+    #     response = requests.get(f'{PIXEL_ENDPOINT}/{user_date}', headers=HEADERS)
+    #     return response.json()['quantity']
+    #
+    # def change_progress_pixel():
+    #
+    #     today = dt.datetime.today().strftime('%Y%m%d')
+    #
+    #     yesterday = dt.datetime.today() - dt.timedelta(days=1)
+    #     yesterday = yesterday.strftime('%Y%m%d')
+    #
+    #     day_before_yesterday = dt.datetime.today() - dt.timedelta(days=1)
+    #     day_before_yesterday = day_before_yesterday.strftime('%Y%m%d')
+    #
+    #     user_date_id = None
+    #
+    #     correct_input = False
+    #     while not correct_input:
+    #         try:
+    #             user_date_id = int(input('Введите код дня, который вы хотите изменить '
+    #                                      '(Сегодня - 0, Вчера - 1, Позавчера - 2): '))
+    #             if user_date_id in (0, 1, 2):
+    #                 correct_input = True
+    #             else:
+    #                 print('Вы ввели неправильный индекс дня. Попробуйте ещё раз.')
+    #         except ValueError:
+    #             print('Некорректный ввод. Попробуйте ещё раз.')
+    #
+    #     if user_date_id == 0:
+    #         print(f'Ваш прогресс сегодня составляет {get_progress_pixel(today)}')
+    #         post_progress_pixel(today)
+    #     elif user_date_id == 1:
+    #         print(f'Ваш прогресс вчера составляет {get_progress_pixel(yesterday)}')
+    #         post_progress_pixel(yesterday)
+    #     elif user_date_id == 2:
+    #         print(f'Ваш прогресс позавчера составляет {get_progress_pixel(day_before_yesterday)}')
+    #         post_progress_pixel(day_before_yesterday)
+    #
+    # change_progress_pixel()
