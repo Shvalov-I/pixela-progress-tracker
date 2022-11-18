514cae1723bfd5c8d9b33937f2a6fda6cc5c03bc
Ivan <shvalov.i@gmail.com>
2022-11-18 08:53:10 +0700

    add engine and session in db models

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, MetaData
+from sqlalchemy.orm import declarative_base, relationship, Session, sessionmaker
 
+engine = create_engine('sqlite:///pixela_tracker_data.db')
 Base = declarative_base()
 
 
@@ -9,7 +10,8 @@ class Users(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True)
     token = Column(String(50), unique=True)
-    graphs = relationship("Graphs", back_populates='user')
+    graphs = relationship("Graphs")
+
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}"
 
@@ -18,5 +20,18 @@ class Graphs(Base):
     __tablename__ = 'pixela_users_graphs'
     id = Column(Integer, primary_key=True)
     graph_name = Column(String(30))
-    user = relationship("Users", back_populates='graphs')
     user_id = Column(Integer, ForeignKey('pixela_users.id'))
+
+
+Base.metadata.create_all(engine)
+
+# with Session(engine) as session:
+#     session.begin()
+#     user_1 = Users(username="CJoker", token="sdfasdf")
+#     graph_1 = Graphs(graph_name='graph1', user_id=1)
+#     graph_2 = Graphs(graph_name='graph2', user_id=2)
+#     session.add(user_1)
+#     session.add(graph_1)
+#     session.add(graph_2)
+#     session.commit()
+
